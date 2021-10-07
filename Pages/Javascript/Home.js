1165d2fd8b27fe59e76851dbe605e59c8cf7464b
Nick <nvaughn@hy-vee.com>
2021-10-07 01:05:39 -0500

    Done

--- Pages/Javascript/Home.js
+++ Pages/Javascript/Home.js
@@ -1,6 +1,6 @@
 let x = -1;
-const directory = 'Formats/Resources/HomePictures/';
-const images = ['img1.jpeg', 'img2.jpeg', 'img3.jpeg', 'img4.jpeg', 'img5.jpeg', 'img6.jpeg'];
+const directory = 'Formats/Resources/';
+const images = ['proj1-1.jpg','proj2-2.jpg','proj3-1.jpg','proj2-1.jpg','proj1-2.jpg','proj3-2.jpg'];
 
 function start() {
     setInterval(displayNext, 2000);
@@ -9,4 +9,5 @@ function start() {
 function displayNext() {
     x = (x === images.length - 1) ? 0 : x + 1;
     document.getElementById('rotatingImage').src = (directory + images[x]);
+    //alert(document.getElementById('rotatingImage').src = (directory + images[x]));
 }
